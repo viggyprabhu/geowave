caeead27c2e69a257a3eed385eb7c6fb0ab363d4
womeara <womeara@radiantblue.com>
2015-03-12 21:19:29 -0700

    Startup Accumulo Monitor with GeoWaveDemoApp

--- geowave-utils/src/main/java/mil/nga/giat/geowave/demo/app/GeoWaveDemoApp.java
+++ geowave-utils/src/main/java/mil/nga/giat/geowave/demo/app/GeoWaveDemoApp.java
@@ -2,8 +2,12 @@ package mil.nga.giat.geowave.demo.app;
 
 import com.google.common.io.Files;
 
+import org.apache.accumulo.core.conf.Property;
 import org.apache.accumulo.minicluster.MiniAccumuloCluster;
 import org.apache.accumulo.minicluster.MiniAccumuloConfig;
+import org.apache.accumulo.minicluster.impl.MiniAccumuloClusterImpl;
+import org.apache.accumulo.minicluster.impl.MiniAccumuloConfigImpl;
+import org.apache.accumulo.monitor.Monitor;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
@@ -32,16 +36,29 @@ public class GeoWaveDemoApp
 						password).setNumTservers(
 						2));
   		else[ACCUMULO_1.5.1]*/
-		final MiniAccumuloCluster accumulo = new MiniAccumuloCluster(
-				new MiniAccumuloConfig(
-						tempDir,
-						password).setNumTservers(
-						2).setInstanceName(
-						instanceName).setZooKeeperPort(
-						2181));
+		final MiniAccumuloConfigImpl miniAccumuloConfig = new MiniAccumuloConfigImpl(
+				tempDir, 
+				password).setNumTservers(
+				2).setInstanceName(
+				instanceName).setZooKeeperPort(
+				2181);
+		
+		miniAccumuloConfig.setProperty(
+				Property.MONITOR_PORT, 
+				"50095");
+		
+		final MiniAccumuloClusterImpl accumulo = new MiniAccumuloClusterImpl(
+				miniAccumuloConfig);
 		/*end[ACCUMULO_1.5.1]*/
 		// @formatter:on
 		accumulo.start();
+
+		// @formatter:off
+		/*if_not[ACCUMULO_1.5.1]
+		accumulo.exec(Monitor.class);
+		end[ACCUMULO_1.5.1]*/
+		// @formatter:on
+
 		System.out.println("starting up ...");
 		Thread.sleep(3000);
 
